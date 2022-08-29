e5c84c104d96d15aa7e0106a39e0bf6f8fc7674a
Gerda <gerda.zykute@oxylabs.io>
2022-08-29 10:40:11 +0300

    remove unused params

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home({text}) {
+export default function Home() {
   return (
     <div className={styles.container}>
       Home page
